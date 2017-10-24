45ad45480b05ddc548d77ea15c0229973e54a7e9
Michelle Chang <mchang98@uw.edu>
2017-10-24 15:32:55 -0700

    completed exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -9,6 +9,21 @@ library("jsonlite")
 
 # How many forks does the most popular repository (by number of forks) have when you search for the term "d3"?
 # (search for the term, and sort descending by the number of forks)
+base.uri <- 'https://api.github.com/search/repositories'
+query.params <- list(q = "d3", sort = "forks", order = "desc")
+response <- GET(base.uri, query = query.params)
+body <- content(response, "text")
+parsed.data <- fromJSON(body)
 
+items <- parsed.data$items
+most.popular.forks <- items[1, 'forks']
 
 # How many public repositories does our course organization have?
+base.uri <- 'https://api.github.com/search/repositories'
+query.params <- list(q = "d3", sort = "forks", order = "desc")
+response <- GET(base.uri, query = query.params)
+body <- content(response, "text")
+parsed.data <- fromJSON(body)
+
+items <- parsed.data$items
+num.repo <-
